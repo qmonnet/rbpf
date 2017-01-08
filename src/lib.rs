3ccb172bc8b198477ad5e8fa57b051ec84170663
Quentin Monnet <quentin.monnet@6wind.com>
2017-01-08 22:48:13 +0100

    src/lib.rs: fix logo URL for the doc

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@
 
 
 //! Virtual machine and JIT compiler for eBPF programs.
-#![doc(html_logo_url = "https://raw.githubusercontent.com/qmonnet/rbpf/master/rbpf.png",
-       html_favicon_url = "https://raw.githubusercontent.com/qmonnet/rbpf/master/rbpf.ico")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/qmonnet/rbpf/master/misc/rbpf.png",
+       html_favicon_url = "https://raw.githubusercontent.com/qmonnet/rbpf/master/misc/rbpf.ico")]
 
 #![warn(missing_docs)]
 
