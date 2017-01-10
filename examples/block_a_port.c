b01b1a8062dbc12407e903331444bc6c52dc4c88
Quentin Monnet <quentin.monnet@6wind.com>
2017-01-10 23:55:40 +0100

    examples: add load_elf example
    This example load a program from an ELF file. The ELF file was compiled
    from C with clang, but had to endure some small edits to run with rbpf.
    
    This example uses two additional dependencies, that are filled under the
    `[dev-dependencies]` tag in the `Cargo.toml`.
    
    Suggested-by: @badboy

---
+++ examples/block_a_port.c
@@ -0,0 +1,38 @@
+#include <linux/ip.h>
+#include <linux/in.h>
+#include <linux/tcp.h>
+#include <linux/bpf.h>
+
+#define ETH_ALEN 6
+#define ETH_P_IP 0x0008 /* htons(0x0800) */
+#define TCP_HDR_LEN 20
+
+#define BLOCKED_TCP_PORT 0x9999
+
+struct eth_hdr {
+    unsigned char   h_dest[ETH_ALEN];
+    unsigned char   h_source[ETH_ALEN];
+    unsigned short  h_proto;
+};
+
+#define SEC(NAME) __attribute__((section(NAME), used))
+SEC(".classifier")
+int handle_ingress(struct __sk_buff *skb)
+{
+    void *data = (void *)(long)skb->data;
+    void *data_end = (void *)(long)skb->data_end;
+    struct eth_hdr *eth = data;
+    struct iphdr *iph = data + sizeof(*eth);
+    struct tcphdr *tcp = data + sizeof(*eth) + sizeof(*iph);
+
+    /* single length check */
+    if (data + sizeof(*eth) + sizeof(*iph) + sizeof(*tcp) > data_end)
+        return 0;
+    if (eth->h_proto != ETH_P_IP)
+        return 0;
+    if (iph->protocol != IPPROTO_TCP)
+        return 0;
+    if (tcp->source == BLOCKED_TCP_PORT || tcp->dest == BLOCKED_TCP_PORT)
+        return -1;
+    return 0;
+}
