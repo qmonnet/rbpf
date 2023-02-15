73ca4e63f1df3f0196d496f09c8456036297f02b
m-tolstrup <mikjen97@gmail.com>
2023-02-15 15:09:26 +0100

    added libfuzzer

--- fuzzer/src/main.rs
+++ fuzzer/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-}
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+extern crate rbpf;
+
+fuzz_target!(|data: &[u8]| {
+    println!("Hello World!");
+});
