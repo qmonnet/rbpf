qmonnet/rbpf

commit ce7d416d9a989919ce5a199a1bf14b06901b61c7

src/jit.rs: Update behaviour for divisions by zero (JIT)
Following the behaviour of Linux, uBPF, and the (in-progress)
specification for the eBPF instruction set, rework the divisions and
modulo remainder when the divisor is zero:

- Dividing by 0 sets the destination register to 0.
- When taking the remainder for the modulo of a division by 0, the
  destination register is unchanged.

See kernel commit f6b1b3bf0d5f ("bpf: fix subprog verifier bypass by
div/mod by 0 exception") for context on this behaviour.

This commit only updates the x86-64 JIT-compiler.

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

src/jit.rs
@@ -253,6 +253,13 @@ fn set_anchor(jit: &mut JitMemory, target: isize) {
     jit.special_targets.insert(target, jit.offset);
 }
 
+#[inline]
+fn emit_direct_jcc(jit: &mut JitMemory, code: u8, offset: u32) {
+    emit1(jit, 0x0f);
+    emit1(jit, code);
+    emit_bytes!(jit, offset, u32);
+}
+
 // Load [src + offset] into dst
 #[inline]
 fn emit_load(jit: &mut JitMemory, size: OperandSize, src: u8, dst: u8, offset: i32) {
@@ -360,8 +367,19 @@ fn muldivmod(jit: &mut JitMemory, pc: u16, opc: u8, src: u8, dst: u8, imm: i32) 
     let div = (opc & ebpf::BPF_ALU_OP_MASK) == (ebpf::DIV32_IMM & ebpf::BPF_ALU_OP_MASK);
     let modrm = (opc & ebpf::BPF_ALU_OP_MASK) == (ebpf::MOD32_IMM & ebpf::BPF_ALU_OP_MASK);
     let is64 = (opc & ebpf::BPF_CLS_MASK) == ebpf::BPF_ALU64;
+    let is_reg = (opc & ebpf::BPF_X) == ebpf::BPF_X;
 
-    if div || modrm {
+    if div && !is_reg && imm == 0 {
+        // Division by zero returns 0
+        // Set register to 0: xor with itself
+        emit_alu32(jit, 0x31, dst, dst);
+        return;
+    }
+    if modrm && !is_reg && imm == 0 {
+        // Modulo remainder of division by zero keeps destination register unchanged
+        return;
+    }
+    if (div || modrm) && is_reg {
         emit_load_imm(jit, RCX, pc as i64);
 
         // test src,src
@@ -371,8 +389,18 @@ fn muldivmod(jit: &mut JitMemory, pc: u16, opc: u8, src: u8, dst: u8, imm: i32) 
             emit_alu32(jit, 0x85, src, src);
         }
 
-        // jz div_by_zero
-        emit_jcc(jit, 0x84, TARGET_PC_DIV_BY_ZERO);
+        if div {
+            // No division by 0: skip next instructions
+            emit_direct_jcc(jit, 0x85, 7);
+            // Division by 0: set dst to 0 then go to next instruction
+            // Set register to 0: xor with itself
+            emit_alu32(jit, 0x31, dst, dst);
+            emit_jmp(jit, (pc + 1) as isize);
+        }
+        if modrm {
+            // Modulo by zero: keep destination register unchanged
+            emit_jcc(jit, 0x84, (pc + 1) as isize);
+        }
     }
 
     if dst != RAX {
@@ -390,7 +418,7 @@ fn muldivmod(jit: &mut JitMemory, pc: u16, opc: u8, src: u8, dst: u8, imm: i32) 
     emit_mov(jit, dst, RAX);
 
     if div || modrm {
-        // xor %edx,%edx
+        // Set register to 0: xor %edx,%edx
         emit_alu32(jit, 0x31, RDX, RDX);
     }
 
@@ -651,7 +679,7 @@ impl<'a> JitMemory<'a> {
                 ebpf::SUB64_REG  => emit_alu64(self, 0x29, src, dst),
                 ebpf::MUL64_IMM | ebpf::MUL64_REG |
                     ebpf::DIV64_IMM | ebpf::DIV64_REG |
-                    ebpf::MOD64_IMM | ebpf::MOD64_REG  =>
+                    ebpf::MOD64_IMM | ebpf::MOD64_REG =>
                     muldivmod(self, insn_ptr as u16, insn.opc, src, dst, insn.imm),
                 ebpf::OR64_IMM   => emit_alu64_imm32(self, 0x81, 1, dst, insn.imm),
                 ebpf::OR64_REG   => emit_alu64(self, 0x09, src, dst),
@@ -662,7 +690,7 @@ impl<'a> JitMemory<'a> {
                     emit_mov(self, src, RCX);
                     emit_alu64(self, 0xd3, 4, dst);
                 },
-                ebpf::RSH64_IMM  =>  emit_alu64_imm8(self, 0xc1, 5, dst, insn.imm as i8),
+                ebpf::RSH64_IMM  => emit_alu64_imm8(self, 0xc1, 5, dst, insn.imm as i8),
                 ebpf::RSH64_REG  => {
                     emit_mov(self, src, RCX);
                     emit_alu64(self, 0xd3, 5, dst);

tests/ubpf_jit_x86_64.rs
@@ -425,11 +425,52 @@ fn test_jit_err_call_unreg() {
     unsafe { vm.execute_program_jit().unwrap(); }
 }
 
-// TODO: Should panic!() instead, but I could not make it panic in JIT-compiled code, so the
-// program returns -1 instead. We can make it write on stderr, though.
 #[test]
-//#[should_panic(expected = "[JIT] Error: division by 0")]
-fn test_jit_err_div64_by_zero_reg() {
+fn test_jit_div64_by_zero_imm() {
+    let prog = assemble("
+        mov32 r0, 1
+        div r0, 0
+        exit").unwrap();
+    let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
+    vm.jit_compile().unwrap();
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x0); }
+}
+
+#[test]
+fn test_jit_div_by_zero_imm() {
+    let prog = assemble("
+        mov32 r0, 1
+        div32 r0, 0
+        exit").unwrap();
+    let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
+    vm.jit_compile().unwrap();
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x0); }
+}
+
+#[test]
+fn test_jit_mod64_by_zero_imm() {
+    let prog = assemble("
+        mov32 r0, 1
+        mod r0, 0
+        exit").unwrap();
+    let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
+    vm.jit_compile().unwrap();
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x1); }
+}
+
+#[test]
+fn test_jit_mod_by_zero_imm() {
+    let prog = assemble("
+        mov32 r0, 1
+        mod32 r0, 0
+        exit").unwrap();
+    let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
+    vm.jit_compile().unwrap();
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x1); }
+}
+
+#[test]
+fn test_jit_div64_by_zero_reg() {
     let prog = assemble("
         mov32 r0, 1
         mov32 r1, 0
@@ -437,13 +478,11 @@ fn test_jit_err_div64_by_zero_reg() {
         exit").unwrap();
     let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
     vm.jit_compile().unwrap();
-    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0xffffffffffffffff); }
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x0); }
 }
 
-// TODO: Same remark as above
 #[test]
-//#[should_panic(expected = "[JIT] Error: division by 0")]
-fn test_jit_err_div_by_zero_reg() {
+fn test_jit_div_by_zero_reg() {
     let prog = assemble("
         mov32 r0, 1
         mov32 r1, 0
@@ -451,13 +490,11 @@ fn test_jit_err_div_by_zero_reg() {
         exit").unwrap();
     let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
     vm.jit_compile().unwrap();
-    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0xffffffffffffffff); }
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x0); }
 }
 
-// TODO: Same remark as above
 #[test]
-//#[should_panic(expected = "[JIT] Error: division by 0")]
-fn test_jit_err_mod64_by_zero_reg() {
+fn test_jit_mod64_by_zero_reg() {
     let prog = assemble("
         mov32 r0, 1
         mov32 r1, 0
@@ -465,13 +502,11 @@ fn test_jit_err_mod64_by_zero_reg() {
         exit").unwrap();
     let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
     vm.jit_compile().unwrap();
-    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0xffffffffffffffff); }
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x1); }
 }
 
-// TODO: Same remark as above
 #[test]
-//#[should_panic(expected = "[JIT] Error: division by 0")]
-fn test_jit_err_mod_by_zero_reg() {
+fn test_jit_mod_by_zero_reg() {
     let prog = assemble("
         mov32 r0, 1
         mov32 r1, 0
@@ -479,7 +514,7 @@ fn test_jit_err_mod_by_zero_reg() {
         exit").unwrap();
     let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
     vm.jit_compile().unwrap();
-    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0xffffffffffffffff); }
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x1); }
 }
 
 // TODO SKIP: JIT disabled for this testcase (stack oob check not implemented)

commit c93e1a2c7105506714aebc102cffb20fd51e39a3

src/jit.rs: Fix offset when skipping insns for handling divisions by 0
When dividing by the content of a register, we have a specific case to
handle divisions by zero. When the denominator is not 0, we skip the
related instructions by emitted a near jump with the relevant offset.

However, this offset was incorrect. We accounted for two instructions: a
2-byte long XOR of the destination register with itself to set it to 0,
and a 5-byte long jump. The former may in fact emit 3 bytes under
certain condition, depending on the destination in use. For example,
when dividing from r7, we emit 3 bytes, and the fixed offset of 7 bytes
we used is incorrect, and triggers a segfault.

To fix this, let's add a check to determine the offset value we should
use. We also add related tests to make sure we don't regress on this in
the future.

Fixes: ce7d416d9a98 ("src/jit.rs: Update behaviour for divisions by zero (JIT)")
Fixes: https://github.com/qmonnet/rbpf/issues/88
Reported-by: Officeyutong
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

src/jit.rs
@@ -148,6 +148,10 @@ impl JitCompiler {
         }
     }
 
+    fn basix_rex_would_set_bits(&self, w: u8, src: u8, dst: u8) -> bool {
+        w != 0 || (src & 0b1000) != 0 || (dst & 0b1000) != 0
+    }
+
     fn emit_rex(&self, mem: &mut JitMemory, w: u8, r: u8, x: u8, b: u8) {
         assert_eq!((w | 1), 1);
         assert_eq!((r | 1), 1);
@@ -159,7 +163,7 @@ impl JitCompiler {
     // Emits a REX prefix with the top bit of src and dst.
     // Skipped if no bits would be set.
     fn emit_basic_rex(&self, mem: &mut JitMemory, w: u8, src: u8, dst: u8) {
-        if w != 0 || (src & 0b1000) != 0 || (dst & 0b1000) != 0 {
+        if self.basix_rex_would_set_bits(w, src, dst) {
             let is_masked = | val, mask | { match val & mask {
                 0 => 0,
                 _ => 1
@@ -397,7 +401,12 @@ impl JitCompiler {
 
             if div {
                 // No division by 0: skip next instructions
-                self.emit_direct_jcc(mem, 0x85, 7);
+                // Jump offset: emit_alu32 adds 2 to 3 bytes, emit_jmp adds 5
+                let offset = match self.basix_rex_would_set_bits(0, dst, dst) {
+                    true => 3 + 5,
+                    false => 2 + 5,
+                };
+                self.emit_direct_jcc(mem, 0x85, offset);
                 // Division by 0: set dst to 0 then go to next instruction
                 // Set register to 0: xor with itself
                 self.emit_alu32(mem, 0x31, dst, dst);

tests/ubpf_jit_x86_64.rs
@@ -392,6 +392,33 @@ fn test_jit_div64_reg() {
     unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x300000000); }
 }
 
+// For some register numbers, we don't emit the same instructions for handling divisions by zero,
+// which means we don't use the same offset to skip these instructions when the divisor is not
+// zero. We've had a regression because of this before, make sure we test it.
+#[test]
+fn test_jit_div32_highreg() {
+    let prog = assemble("
+        mov r0, 2
+        mov r7, 4
+        div32 r7, r0
+        exit").unwrap();
+    let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
+    vm.jit_compile().unwrap();
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x2); }
+}
+
+#[test]
+fn test_jit_div64_highreg() {
+    let prog = assemble("
+        mov r0, 2
+        mov r7, 4
+        div r7, r0
+        exit").unwrap();
+    let mut vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
+    vm.jit_compile().unwrap();
+    unsafe { assert_eq!(vm.execute_program_jit().unwrap(), 0x2); }
+}
+
 #[test]
 fn test_jit_early_exit() {
     let prog = assemble("